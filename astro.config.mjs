e938a346358cf298f44887896db372743f550f3e
PeopleArtFactory <info@peopleartfactory.com>
2024-07-26 20:03:02 +0200

    new test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -50,8 +49,6 @@ export default defineConfig({
           autogenerate: { directory: "developers" },
         },
       ],
-      customCss: ["./src/tailwind.css"],
-    }),
-    tailwind({ applyBaseStyles: false }),
+    })
   ],
 });
